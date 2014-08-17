3991273665095f3b6c2a006a6d9262257d99b9bb
Rabbit <paul.szulc@gmail.com>
2014-08-17 17:46:15 +0200

    P01: returns none for empty list

--- src/main/scala/com/rabbitonweb/nnspc/p01/P01.scala
+++ src/main/scala/com/rabbitonweb/nnspc/p01/P01.scala
@@ -1,5 +1,8 @@
 package com.rabbitonweb.nnspc.p01
 
 class P01[T] {
-  def last(list: List[T]): T = list(0)
+  def last(list: List[T]): Option[T] = list match {
+    case Nil => None
+    case _ => Some(list(0))
+  }
 }
--- src/test/scala/com/rabbitonweb/nnspc/p01/P01Test.scala
+++ src/test/scala/com/rabbitonweb/nnspc/p01/P01Test.scala
@@ -7,6 +7,10 @@ class P01Test extends FunSuite with Matchers {
   val p01 = new P01[Int]()
 
   test("that returns element for single element's list") {
-    p01.last(List(3)) should equal(3)
+    p01.last(List(3)) should equal(Some(3))
+  }
+
+  test("that returns none for empty list") {
+    p01.last(List()) should be(None)
   }
 }
