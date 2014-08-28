3146b2adc8dec4f9d70bb6d2cbde979a9d2963c5
Rabbit <paul.szulc@gmail.com>
2014-08-28 23:11:37 +0200

    P02: tailrec added

--- src/main/scala/com/rabbitonweb/nnspc/p02/P02.scala
+++ src/main/scala/com/rabbitonweb/nnspc/p02/P02.scala
@@ -1,7 +1,11 @@
 package com.rabbitonweb.nnspc.p02
 
+import scala.annotation.tailrec
+
 class P02[T] {
-  def penultimate(list: List[T]): Option[T] = list match {
+
+  @tailrec
+  final def penultimate(list: List[T]): Option[T] = list match {
     case Nil => None
     case head :: Nil => None
     case lastButOne :: last :: Nil => Some(lastButOne)
