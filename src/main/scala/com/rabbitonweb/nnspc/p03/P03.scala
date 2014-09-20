c79a52e2c38e663b9d6e84826e6ae8f32f17abf4
Rabbit <paul.szulc@gmail.com>
2014-09-20 22:35:54 +0200

    P02: that fails if nth bigger then size of the list

--- src/main/scala/com/rabbitonweb/nnspc/p03/P03.scala
+++ src/main/scala/com/rabbitonweb/nnspc/p03/P03.scala
@@ -1,5 +1,5 @@
 package com.rabbitonweb.nnspc.p03
 
 class P03[T] {
-  def nth(nth: Int, list: List[T]): T = list(nth)
+  def nth(nth: Int, list: List[T]): Option[T] = if (nth < list.size) Some(list(nth)) else None
 }
--- src/test/scala/com/rabbitonweb/nnspc/p03/P03Test.scala
+++ src/test/scala/com/rabbitonweb/nnspc/p03/P03Test.scala
@@ -6,6 +6,10 @@ class P03Test extends FunSuite with Matchers {
   val p03 = new P03[Int]()
 
   test("that returns nth element") {
-    p03.nth(0, List(7, 6, 2, 3, 5, 8)) should be(7)
+    p03.nth(0, List(7, 6, 2, 3, 5, 8)) should be(Some(7))
+  }
+
+  test("that fails if nth bigger then size of the list") {
+    p03.nth(30, List(7, 6, 2, 3, 5, 8)) should be(None)
   }
 }
