febaa9111726d70d396613412142da4e5b99db94
Rabbit <paul.szulc@gmail.com>
2014-09-20 22:47:37 +0200

    P03 - implementation change not to use built-in api

--- src/main/scala/com/rabbitonweb/nnspc/p03/P03.scala
+++ src/main/scala/com/rabbitonweb/nnspc/p03/P03.scala
@@ -1,6 +1,9 @@
 package com.rabbitonweb.nnspc.p03
 
 class P03[T] {
-  def nth(nth: Int, list: List[T]): Option[T] =
-    if (nth < list.size && nth >= 0) Some(list(nth)) else None
+  def nth(n: Int, list: List[T]): Option[T] = (n, list) match {
+    case (0, head :: tail) => Some(head)
+    case (_, Nil) => None
+    case (x, head :: tail) => if (x < 0) None else nth(x - 1, tail)
+  }
 }
