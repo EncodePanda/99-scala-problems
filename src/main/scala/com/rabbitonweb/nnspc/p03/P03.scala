47accc8b537ad7840ed607639c0bb19a1d10aa0a
Rabbit <paul.szulc@gmail.com>
2014-09-20 22:37:29 +0200

    P02: that fails if nth smaller then 0

--- src/main/scala/com/rabbitonweb/nnspc/p03/P03.scala
+++ src/main/scala/com/rabbitonweb/nnspc/p03/P03.scala
@@ -1,5 +1,6 @@
 package com.rabbitonweb.nnspc.p03
 
 class P03[T] {
-  def nth(nth: Int, list: List[T]): Option[T] = if (nth < list.size) Some(list(nth)) else None
+  def nth(nth: Int, list: List[T]): Option[T] =
+    if (nth < list.size && nth >= 0) Some(list(nth)) else None
 }
--- src/test/scala/com/rabbitonweb/nnspc/p03/P03Test.scala
+++ src/test/scala/com/rabbitonweb/nnspc/p03/P03Test.scala
@@ -12,4 +12,8 @@ class P03Test extends FunSuite with Matchers {
   test("that fails if nth bigger then size of the list") {
     p03.nth(30, List(7, 6, 2, 3, 5, 8)) should be(None)
   }
+
+  test("that fails if nth smaller then 0") {
+    p03.nth(-1, List(7, 6, 2, 3, 5, 8)) should be(None)
+  }
 }
