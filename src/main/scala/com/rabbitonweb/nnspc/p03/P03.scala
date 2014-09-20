b3ee79a21ba6bbf60af76d239ecd1866f8a2c28a
Rabbit <paul.szulc@gmail.com>
2014-09-20 22:54:12 +0200

    P03 - add @tailrec, also case matching given integer value (x < 0)

--- src/main/scala/com/rabbitonweb/nnspc/p03/P03.scala
+++ src/main/scala/com/rabbitonweb/nnspc/p03/P03.scala
@@ -1,9 +1,14 @@
 package com.rabbitonweb.nnspc.p03
 
+import scala.annotation.tailrec
+
 class P03[T] {
-  def nth(n: Int, list: List[T]): Option[T] = (n, list) match {
+
+  @tailrec
+  final def nth(n: Int, list: List[T]): Option[T] = (n, list) match {
     case (0, head :: tail) => Some(head)
     case (_, Nil) => None
-    case (x, head :: tail) => if (x < 0) None else nth(x - 1, tail)
+    case (x, _) if x < 0 => None
+    case (x, head :: tail) => nth(x - 1, tail)
   }
 }
