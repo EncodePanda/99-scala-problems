f62d8d28627744610902694677a7a03cd31c8a82
Paweł Szulc <paul.szulc@gmail.com>
2015-03-11 15:54:49 +0200

    switch from gradle to sbt

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "ninety-nine-scala-problems-challenge"
+
+scalaVersion := "2.11.5"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
--- src/test/scala/com/rabbitonweb/nnspc/p01/P01Test.scala
+++ src/test/scala/com/rabbitonweb/nnspc/p01/P01Test.scala
@@ -1,10 +1,8 @@
 package com.rabbitonweb.nnspc.p01
 
-import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FunSuite, Matchers}
 
-@RunWith(classOf[JUnitRunner])
 class P01Test extends FunSuite with Matchers {
 
   val p01 = new P01[Int]()
