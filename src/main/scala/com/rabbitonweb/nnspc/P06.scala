0974654e9445b955d5c48593161dc583441bbd3f
Paweł Szulc <paul.szulc@gmail.com>
2015-03-13 22:34:19 +0100

    P06 solved with own revers

--- src/main/scala/com/rabbitonweb/nnspc/P06.scala
+++ src/main/scala/com/rabbitonweb/nnspc/P06.scala
@@ -7,5 +7,13 @@ scala> isPalindrome(List(1, 2, 3, 2, 1))
 res0: Boolean = true
  */
 class P06[T] {
-  def isPalindrome(list: List[T]): Boolean = list == list.reverse
+  def isPalindrome(list: List[T]): Boolean = list == reverse(list)
+
+  def reverse(list: List[T]): List[T] = {
+    def inner(l: List[T], acc: List[T]): List[T] = l match {
+      case Nil => acc
+      case head :: tail => inner(tail, head :: acc)
+    }
+    inner(list, Nil)
+  }
 }
